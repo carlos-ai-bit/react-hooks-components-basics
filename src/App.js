1d3849d6258e226fae1bed2b3b71a70a4c6f925f
Hoperaisers <abdallahsafari@gmail.com>
2023-05-22 10:47:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+}
+
+export default Navbar;
